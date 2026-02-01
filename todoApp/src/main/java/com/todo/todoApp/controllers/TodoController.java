7821f7fe4aa640118c6b2139fc055aaeb81703fc
aryandhirhi004 <aryandhirhi004@gmail.com>
2026-02-01 16:07:41 +0530

    feat: Todo controller for todo registration

---
+++ todoApp/src/main/java/com/todo/todoApp/controllers/TodoController.java
@@ -0,0 +1,38 @@
+package com.todo.todoApp.controllers;
+
+import com.todo.todoApp.advice.ApiResponse;
+import com.todo.todoApp.dto.TodoDTO;
+import com.todo.todoApp.services.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/todos")
+public class TodoController {
+
+    private final TodoService todoService;
+
+    @PostMapping("/register-todo")
+    public ResponseEntity<ApiResponse<TodoDTO>> registerTodo(@RequestBody TodoDTO todoDTO){
+        try {
+            TodoDTO todo = todoService.registerTodo(todoDTO);
+            return ResponseEntity.ok(new ApiResponse<>(todo));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @GetMapping("/get-todo-by-id/{todoId}")
+    public ResponseEntity<ApiResponse<TodoDTO>> getTodoById(@RequestParam String todoId){
+        try{
+            TodoDTO todo = todoService.getTodoById(todoId);
+            return ResponseEntity.ok(new ApiResponse<>(todo));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
---
+++ todoApp/src/main/java/com/todo/todoApp/dto/TodoDTO.java
@@ -0,0 +1,4 @@
+package com.todo.todoApp.dto;
+
+public class TodoDTO {
+}
